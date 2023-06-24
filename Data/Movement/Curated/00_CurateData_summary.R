39dacd029a013458988ce614924871c70a738f16
edmizac <duhmzc@gmail.com>
2023-06-24 17:05:38 -0300

    estimated number of resting+idle scans for resting parameterization

--- Data/Movement/Curated/00_CurateData_summary.R
+++ Data/Movement/Curated/00_CurateData_summary.R
@@ -18,6 +18,7 @@ library("tidyverse")
 library("dplyr")
 library("ggplot2")
 
+theme_set(theme_bw(base_size = 16))
 
 # Read data
 dat.all <- read.csv(here("Data", "Movement", "Curated", "BLT_groups_data.csv")
@@ -30,14 +31,78 @@ dat.all <- read.csv(here("Data", "Movement", "Curated", "BLT_groups_data.csv")
 dat.all$id_day_all %>% levels()
 dat.all$id_month %>% levels()
 
+# Explore when tamarins rest
+target <- c("Resting", "Inactive")
+rest_data <- dat.all %>% 
+  mutate(
+    datetime = ymd_hms(datetime)
+  ) %>% 
+  mutate(
+    date = as.Date(datetime),
+    time = hms::as_hms(datetime)
+  ) %>% 
+  # group_by(group, id_month, date) %>% 
+  group_by(group, id_month) %>% 
+  mutate(
+    count = n() # timesteps
+  ) %>% 
+  dplyr::filter(behavior %in% target) %>% 
+  mutate(
+    count_rest = n()
+  )
+
+  
+lims <-  c("06:00", "18:00") %>% as_hms()
+
+rest_data %>% 
+  ggplot() +
+  geom_density(aes(x = time, color = behavior), 
+               # adjust = 5,
+               linewidth = 0.7) +
+  # geom_jitter(aes(x= time, y = 0, height = 0.001, color = behavior), 
+  # geom_point(aes(x= time, y = 0, color = behavior), 
+  #             size = 0.7, alpha = 0.5) +
+  geom_rug(aes(x = time, color = behavior), linewidth = 0.4,
+           length = unit(0.05, "npc")
+           ) +
+  facet_wrap(~group+id_month) +
+  ggtitle("Probability of resting or idle") +
+  scale_x_time(breaks = scales::breaks_width("2 hours"), limits = lims) +
+  theme(
+    axis.text.x = element_text(
+      # size = 12,
+      angle = 90
+    )
+  ) +
+  scale_color_manual(values = c("#2E86C1", "#D35400")) +
+  ggpp::geom_text_npc(aes(label=paste("n=", count_rest, "/", count)),
+                      npcx = "center", npcy = "top"
+  )
+
+# # Save plot
+# ggsave(here("Data", "Movement", "Curated", "Param_siminputrow",
+#             '00_Resting_probability1.png'), height = 7, width = 10, dpi = 600)
+
+
+
 # Summarise important variables
 dat.summary <- dat.all %>% 
-  group_by(group, id_month) %>% 
+  group_by(group, id_month) %>%
   dplyr::summarise(
     timesteps = n(),
     ndays = n_distinct(id_day_all),
     mean_timesteps = round(timesteps/ndays)
-  ) 
+  )
+
+# Attach resting+idle counts
+rest_data_summary <- rest_data %>% 
+  dplyr::select(group, id_month, count_rest) %>% 
+  distinct()
+
+dat.summary <- dat.summary %>% 
+  dplyr::left_join(rest_data_summary, by = c("group", "id_month"))
+
+
 
 # Save csv:
 # dat.summary %>%
